f647d1405ba150ae5bab90e6a2f94b06ddd4ba5e
N1ve5h <46673471+N1ve5h@users.noreply.github.com>
2019-03-06 14:49:40 +0000

    Using Switch statment and break statement in form.

---
+++ wk3ex3action.php
@@ -0,0 +1,13 @@
+<?php
+  switch ( $_POST["radcourse"])
+  {
+    case "HNC" : echo "You have selected a HNC course";
+        
+    case "HND" : echo "You have selected a HND course";
+
+    case "BSC" : echo "You have selected a BSC course";
+
+    case "PTBSC" : echo "You have selected a Part Time BSC course";
+
+  }
+?>
