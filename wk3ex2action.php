b1efee49fa1d069a657137bffd63dc57bc31c18f
N1ve5h <46673471+N1ve5h@users.noreply.github.com>
2019-03-06 14:30:10 +0000

    Age and gender form

--- wk3ex2action.php
+++ wk3ex2action.php
@@ -1,10 +1,20 @@
 <?php
-  if ( $_POST["txtage"] < 21)
+
+  if ( $_POST["txtage"] < 21) //if statement age
   {
     echo "You are under 21 years old <br/>";
   }
   else
   {
     echo "You are 21 years old or over<br/>";
+  }
+
+  if ( $_POST["txtgender"] == "Male") //if statement gender
+  {
+    echo "You are a Male";
+  }
+  else if ( $_POST["txtgender"] == "Female")
+  {
+    echo "You are a Female";
   }
 ?>
