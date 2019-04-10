ca579056d1ad806976ce299c0a12ef54b7741b06
N1ve5h <46673471+N1ve5h@users.noreply.github.com>
2019-04-10 15:27:53 +0100

    Added if statement based on what button is pressed

--- wk6ex3action.php
+++ wk6ex3action.php
@@ -1,5 +1,13 @@
 <?php
   $link = mysqli_connect('127.0.0.1', 'root', '', 'db1_21706089');
-  $sql = "UPDATE test SET phone_number='$_GET[txttelno]',email='$_GET[txtemail]' WHERE name='$_GET[txtname]'";
+  if ($_GET['btnsubmit'] == "save")
+  {
+    $sql = "UPDATE test SET phone_number='$_GET[txttelno]',email='$_GET[txtemail]' WHERE name='$_GET[txtname]'";
+  }
+  else
+  {
+    $sql = "DELETE FROM test WHERE name = '$_GET[txtname]'";  
+  }
+
   $result=mysqli_query($link,$sql);
 ?>
