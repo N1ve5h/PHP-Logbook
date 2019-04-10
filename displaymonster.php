100388301fe3f97da4d67cd36275dc21de0eaa10
N1ve5h <46673471+N1ve5h@users.noreply.github.com>
2019-04-10 20:43:14 +0100

    Make it look clean

--- displaymonster.php
+++ displaymonster.php
@@ -1,17 +1,15 @@
 <?php
 
   $conn = mysqli_connect('127.0.0.1', 'root', '', 'db1_21706089');
-
-$sql = "SELECT id from monster;";
-
-$result = mysqli_query($conn,$sql);
-$row = mysqli_fetch_array($result);
-include ('myfunctions.inc');
-html_header("Monster");
-while ($row = mysqli_fetch_assoc($result))
-{
-  echo "<img src='getjpg.php?id=" . $row[id]. "'/>";
-}
-html_footer();
-mysqli_close($conn);
+  $sql = "select id,name from monster;";
+  $result = mysqli_query($conn,$sql);
+  $row = mysqli_fetch_array($result);
+  include ('myfunctions.inc');
+  html_header("Monster");
+  while ($row = mysqli_fetch_assoc($result))
+  {
+    echo "<img src='getjpg.php?id=" . $row[id]. "'/>";
+  }
+  html_footer();
+  mysqli_close($conn);
 ?>
