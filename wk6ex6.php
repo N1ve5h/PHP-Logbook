b1051b5a51aef8c998396eac3342ca15e85df97e
N1ve5h <46673471+N1ve5h@users.noreply.github.com>
2019-04-10 15:56:50 +0100

    Added tex allowance

--- wk6ex6.php
+++ wk6ex6.php
@@ -1,6 +1,6 @@
 <?php
   include("myfunctions.inc");
   html_header("My second function demo");
-  echo "I pay  £". calculatetax(15000) . " tax";
+  echo "I pay  £". calculatetax(15000,5000) . " tax"; //10000 will be taxed by 40%
   html_footer();
 ?>
